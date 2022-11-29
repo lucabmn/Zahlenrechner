e97556fc7a7990cea4b688702f3d50ea38868ae3
Luca <65792217+lucabmn@users.noreply.github.com>
2022-11-29 10:20:33 +0100

    Add files via upload

--- rechner.php
+++ rechner.php
@@ -4,9 +4,9 @@
 </head>
 <body>
 <?php
-    $zahl = $_POST["Zahl"];
-    $rechner1 = $_POST["rechner1"];
-    $rechner2 = $_POST["rechner2"];
+    $zahl = $_GET["Zahl"];
+    $rechner1 = $_GET["rechner1"];
+    $rechner2 = $_GET["rechner2"];
     
     if ($rechner1 == "Binär") {
         $zahl = bindec($zahl);
